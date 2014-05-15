22bb3ce9bfa71bdb40b8b3abb8efd32bd19186d9
理貴 <b1008244@gmail.com>
2014-05-16 01:25:18 +0900

    Scene2を追加

---
+++ UnityAnimation/Assets/enterKeyObjectStop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enterKeyObjectStop : MonoBehaviour {
+
+	void Start () {	
+		GameObject.Find("GUI Text").guiText.text = "AnimationClip sample";
+		AnimationClip clip = new AnimationClip();
+		AnimationCurve curve = AnimationCurve.Linear(0f, 3f, 3f, 3f);
+		Keyframe key = new Keyframe(1.5f,7f);
+		curve.AddKey(key);
+		clip.SetCurve("", typeof(Transform), "localPosition.z", curve);
+		clip.wrapMode = WrapMode.Loop;
+		animation.AddClip(clip, "clip1");
+		animation.Play("clip1");
+	}
+	
+	void Update () {
+		transform.Rotate(1f, 1f, 1f);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			animation.Stop ();
+			GameObject.Find("GUI Text").guiText.text = "Stopped!!";
+		}
+		if(Input.GetKeyUp(KeyCode.Space)){
+			animation.Play("clip1");
+			GameObject.Find("GUI Text").guiText.text = "Animation sampled";
+		}
+	}
+}
